b804e85a11e6684c6a7e23e6cc30ba1123328178
heerokj <wjdgmlfhr0321@gmail.com>
2026-03-23 22:52:48 +0900

    feat : 카페 앤드포인트 추가

---
+++ src/routes/cafes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const { XMLParser } = require("fast-xml-parser");
+
+const parser = new XMLParser();
+
+// GET  /api/cafes
+router.get("/", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.kcisa.kr/openapi/API_CIA_090/request?serviceKey=${process.env.CAFE_SERVICE_KEY}&numOfRows=5&pageNo=20`
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        success: false,
+        message: "공공데이터 API 오류",
+      });
+    }
+
+    const xml = await response.text();
+    const json = parser.parse(xml);
+
+    return res.status(200).json(json);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "서버 오류가 발생했습니다",
+      error: error.message,
+    });
+  }
+});
+
+// GET  /api/cafes?keyword=검색어&pageNo=1&numOfRows=6
+router.get("/", async (req, res) => {
+  const cafeName = req.query.keyword || "";
+  const pageNo = Number(req.query.pageNo) || 1;
+  const numOfRows = Number(req.query.numOfRows) || 5;
+
+  const start = (pageNo - 1) * numOfRows + 1;
+  const to = pageNo * numOfRows;
+
+  try {
+    const response = await fetch(
+      `https://api.kcisa.kr/openapi/API_CIA_090/request?serviceKey=${
+        process.env.CAFE_SERVICE_KEY
+      }&keyword=${encodeURIComponent(
+        cafeName
+      )}&numOfRows=${numOfRows}&pageNo=${start}`
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        success: false,
+        message: "공공데이터 API 오류",
+      });
+    }
+
+    const xml = await response.text();
+    const json = parser.parse(xml);
+
+    const items = json.response.body.items.item ?? [];
+    const totalCount = json.response.body.totalCount;
+    const hasNext = to < (totalCount || 0);
+
+    return res.status(200).json({
+      data: items,
+      count: totalCount,
+      hasNext,
+      nextPage: pageNo + 1,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "서버 오류가 발생했습니다",
+      error: error.message,
+    });
+  }
+});
+
+module.exports = router;
